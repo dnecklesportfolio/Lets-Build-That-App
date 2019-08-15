2197c81f94854c36e210a9b71e6f81bf19695c01
Dwayne Neckles <dwayneaneckles@gmail.com>
2019-08-15 14:14:02 -0400

    added 18 cells

--- StretchyHeaderLBTA/StretchyHeaderLBTA/StretchyHeaderController.swift
+++ StretchyHeaderLBTA/StretchyHeaderLBTA/StretchyHeaderController.swift
@@ -12,13 +12,24 @@ import UIKit
 class StretchyHeaderController: UICollectionViewController {
 
     fileprivate let cellID = "cellID"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
        collectionView.backgroundColor = .yellow
         
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellID)
         
     }
     
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 18
+    }
     
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellID, for: indexPath)
+        cell.backgroundColor =   .black
+        return cell
+    }
 }
