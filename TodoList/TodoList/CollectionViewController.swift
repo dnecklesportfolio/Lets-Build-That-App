ad8a3b95f4df934f3400001b138fed83c8a6de91
Dwayne Neckles <dwayneaneckles@gmail.com>
2019-08-14 20:31:46 -0400

    first commit: A few dummy cells are displayed

--- TodoList/TodoList/CollectionViewController.swift
+++ TodoList/TodoList/CollectionViewController.swift
@@ -19,13 +19,46 @@ class CollectionViewController: UICollectionViewController {
         collectionView?.backgroundColor = .white
         collectionView?.alwaysBounceVertical = true
         
-        
+        collectionView?.register(TaskCell.self, forCellWithReuseIdentifier: "cell")
         
         
     }
     
-   
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 20
+    }
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        return collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        
+    }
     
+    
+    
+}
+
+
+class TaskCell: UICollectionViewCell {
+    override init(frame:CGRect) {
+        super.init(frame: frame)
+        setupViews()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // this is a closure
+    let nameLabel:UILabel = {
+        let label = UILabel()
+        label.text = "Sample Task"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label}()
+    
+    func setupViews() {
+        addSubview(nameLabel)
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[v0]|", options: NSLayoutConstraint.FormatOptions(), metrics: nil, views: ["v0":nameLabel]))
+    }
     
 }
 
