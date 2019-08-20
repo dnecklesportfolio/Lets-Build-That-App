3dcfb43ec03aec7cc77f82b09e2bb62c76b8d9a1
Dwayne Neckles <dwayneneckles@Dwaynes-MBP.home>
2019-08-19 21:28:42 -0400

    Appstore First Commit created stubs for sections
    Created three viewcontrollers then replace one with a custom class. Also refactored code to reduce duplication. Currently shows three sections and a navbar on bottom and title on top

---
+++ AppStoreJSONAPis/AppStoreJSONAPis/Controller/AppSearchController.swift
@@ -0,0 +1,40 @@
+//
+//  AppSearchController.swift
+//  AppStoreJSONAPis
+//
+//  Created by Dwayne Neckles on 8/19/19.
+//  Copyright © 2019 Dwayne Neckles. All rights reserved.
+//
+
+import UIKit
+
+class AppsSearchController : UICollectionViewController {
+    
+    fileprivate let cellId = "id1234"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView.backgroundColor = .white
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellId)
+    }
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+    cell.backgroundColor = .red
+    return cell}
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    init() {
+        super.init(collectionViewLayout: UICollectionViewFlowLayout())
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    }
+    
+
+
+
